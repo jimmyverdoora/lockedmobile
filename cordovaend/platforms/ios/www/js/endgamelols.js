282e6b7f8fcccfd27383f5596ceb9cec72f3ac94
jimmy <micheleroselli94@gmail.com>
2020-01-10 12:32:46 +0100

    RESETTING PLATFORMS

--- cordovaend/platforms/ios/www/js/endgamelols.js
+++
@@ -1,7 +0,0 @@
-var winQuotes = ["Better not tell Donald you are that strong...",
-                 "Chuck Norris, watch out!",
-];
-
-var loseQuotes = ["C'mon, we all know they cheated...",
-                  "Next time, better call Saul!",
-]
