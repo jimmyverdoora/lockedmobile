a3824d2c1223414feb3d1833c436f0a842ad86ae
jimmy <micheleroselli94@gmail.com>
2020-01-10 16:58:41 +0100

    20 quotes!

--- cordovaend/www/js/endgamelols.js
+++ cordovaend/www/js/endgamelols.js
@@ -1,7 +1,23 @@
 var winQuotes = ["Better not tell Donald you are that strong...",
                  "Chuck Norris, watch out!",
+                 "To infinity and beyonddddddd!",
+                 "Here's Johnny!",
+                 "Izi-mentary, my dear Watson.",
+                 "Your are not in danger, you are the danger.",
+                 "Locked is cool. Change my mind.",
+                 "Such win. Such wow...",
+                 "What if I told you... GG M8!",
+                 "One tris, to rule them all."
 ];
 
-var loseQuotes = ["C'mon, we all know they cheated...",
+var loseQuotes = ["Next time, just use the Force...",
                   "Next time, better call Saul!",
-]
+                  "The first rule of Locked is: you don't talk about Locked.",
+                  "C'mon, why so serious?",
+                  "We told'em: you'll be back.",
+                  "Choice. The problem is choice.",
+                  "You don't always lose, but when I do (they cheated)",
+                  "Next time, try playing on the Nokia 3310 ;)",
+                  "Well then. It's hacking time.",
+                  "When you play the game of Locked, you win or you rage quit."
+];
