7d6ac831f2345651bd53bfbe7e40a2b60a9f4e8a
jimmy <micheleroselli94@gmail.com>
2020-01-13 08:46:18 +0100

    Added logs to website

--- website/website/views.py
+++ website/website/views.py
@@ -1,4 +1,16 @@
+from website.settings import LOG_FILE
 from django.shortcuts import render
+from logging.handlers import TimedRotatingFileHandler
+import logging
+
+logging.basicConfig(level=logging.WARN, format='%(levelname)s %(asctime)s %(message)s')
+LOGGERONE = logging.getLogger("WebsiteLogger")
+handler = TimedRotatingFileHandler(LOG_FILE, when="midnight", interval=1, backupCount=7)
+formatter = logging.Formatter('%(levelname)s %(asctime)s %(message)s')
+handler.setFormatter(formatter)
+LOGGERONE.setHandler(handler)
+LOGGERONE.setLevel(logging.INFO)
+LOGGERONE.propagate = False
 
 
 def homePageView(request):
@@ -6,6 +18,7 @@ def homePageView(request):
     try:
         r = requests.get('http://lockedapi.advenagames.com:8080/version')
         if r.status_code != 200:
+            LOGGERONE.error(r.__dict__)
             raise Exception()
         context = r.json()
     except Exception:
